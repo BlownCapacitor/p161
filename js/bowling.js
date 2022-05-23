876098ae8a01ccf139304efe4bcb221e10f0bc62
BlownCapacitor <52745475+BlownCapacitor@users.noreply.github.com>
2022-05-23 11:06:18 +0530

    added stuff for p162

--- js/bowling.js
+++ js/bowling.js
@@ -14,20 +14,52 @@ AFRAME.registerComponent("ball", {
                 var cam = document.querySelector("#camera");
                 pos = cam.getAttribute("position");
                 ball.setAttribute("position", {
-                    x: pos.x, y: pos.y, z: pos.z
+                    x: pos.x, y: pos.y-1.5, z: pos.z
 
                 })
                 var cam2 = document.querySelector("#camera").object3D;
                 var direction = new THREE.Vector3();
                 cam2.getWorldDirection(direction)
-                ball.setAttribute("velocity", direction.multiplyScalar(-5))
+                ball.setAttribute("velocity", direction.multiplyScalar(-10))
                 console.log(direction)
                 var scene = document.querySelector("#scene");
-                
+
+                ball.setAttribute("dynamic-body", {
+                    shape: "sphere",
+                    mass: "0.51",
+                  });
+
+                  ball.addEventListener("collide", this.removeBall);
+          
+
                 scene.appendChild(ball)
             }
         })
-    }
-
-
-})
+    },
+    removeBall: function (e) {
+        var ballcollide = e.detail.target.el;
+        var hit = e.detail.body.el;
+        if (hit.id.includes("pin")) {
+          hit.setAttribute("material", {
+            opacity: 0,
+            transparent: true,
+    
+          });
+          hit.setAttribute("dynamic-body",{
+            mass:0.1
+          })
+          var impulse = new CANNON.Vec3(0.1, 0.1, 0.1)
+          var worldPoint = new CANNON.Vec3().copy(
+            hit.getAttribute("position")
+          );
+          hit.body.applyImpulse(impulse, worldPoint);
+        ball.setAttribute("material",{
+          opacity: 0,
+          transparent:true,
+        })
+        ballcollide.removeEventListener("collide", this.roll);
+        var sceneRemove = document.querySelector("scene");
+        sceneRemove.removeChild(ball);
+          }
+      },
+      })
